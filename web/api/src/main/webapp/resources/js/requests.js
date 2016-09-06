7ec7082feb7ed6d68bcae9a54c77aa88b3f6d86b
copperkaft <copperkraft@gmail.com>
2016-09-06 17:56:30 +0300

    Change table

--- web/api/src/main/webapp/resources/js/requests.js
+++ web/api/src/main/webapp/resources/js/requests.js
@@ -16,11 +16,29 @@ function changeWelcome() {
 }
 function loadGroups() {
     var getGroupsInfo = new XMLHttpRequest();
-    getGroupsInfo.open("GET", "user/getName", true);  //Указываем адрес GET-запроса
+    getGroupsInfo.open("GET", "user/GetGroups", true);  //Указываем адрес GET-запроса
     getGroupsInfo.onload = function (){ //Функция которая отправляет запрос на сервер для получения имени пользователя
-        var parsedName = JSON.parse(this.responseText); //получаем объект их JSON ответа.
-        document.getElementById('userName').innerHTML =
-            'Приветствуем вас, ' + parsedName.userName + ' ' + parsedName.userLastName + '!';
+        var parsedGroupsInfo = JSON.parse(this.responseText); //получаем объект их JSON ответа.
+        var groupTable = document.getElementById('all-students');
+        for (var i = 0; i < parsedGroupsInfo.length; i++) {
+            var tableRowElement = document.createElement('tr');
+            //groupName, groupAvatar
+            var groupAvatarElement = document.createElement('td');
+            groupAvatarElement.style.background = 'url(' + parsedGroupsInfo.groupAvatar + ')';
+            groupAvatarElement.width = '100px';
+            groupAvatarElement.height = '100px';
+            tableRowElement.appendChild(groupAvatarElement);
+
+            var groupNameElement = document.createElement('td');
+            groupNameElement.innerHTML = parsedGroupsInfo.groupName;
+            tableRowElement.appendChild(groupNameElement);
+
+            var managementElement = document.createElement('td');
+            var managementLinkElement = document.createElement('a');
+            managementLinkElement.innerHTML = 'Управлять группой';
+            managementLinkElement.href = "manage-groups.html";
+            managementElement.appendChild(managementLinkElement);
+        }
     };
     getGroupsInfo.send(null);
 }
