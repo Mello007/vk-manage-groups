3301970b876874e8590b532dd018b2d5444e63a9
Владимир <copperkraft@gmail.com>
2016-09-21 21:54:38 +0300

    Load group changed

--- web/api/src/main/webapp/resources/js/requests.js
+++ web/api/src/main/webapp/resources/js/requests.js
@@ -20,12 +20,24 @@ function changeWelcome() {
 }
 
 function loadGroups() {
+    //parameters of json object: name, avatar (there is address in vk), id group. That's all.
     var getGroupsInfo = new XMLHttpRequest();
-    getGroupsInfo.open("GET", "user/getName", true);  //Указываем адрес GET-запроса
+    getGroupsInfo.open("GET", "group/get", true);  //Указываем адрес GET-запроса
     getGroupsInfo.onload = function () { //Функция которая отправляет запрос на сервер для получения имени пользователя
-        var parsedName = JSON.parse(this.responseText); //получаем объект их JSON ответа.
-        document.getElementById('userName').innerHTML =
-            'Приветствуем вас, ' + parsedName.userName + ' ' + parsedName.userLastName + '!';
+        var parsedName = JSON.parse(this.responseText), //получаем объект их JSON ответа.
+            tableElement = document.getElementById('all-groups');
+        tableElement.innerHTML = '';
+        parsedName.forEach(function (group) {
+            var tableRowElement = document.createElement('tr'),
+                imgContainer = document.createElement('td'),
+                nameContainer = document.createElement('td');
+            imgContainer.innerHTML = '<img src="' + group.avatar + '" class = "img-circle" alt = "' + group.name +
+                    '" width="50" height="50">';
+            nameContainer.innerHTML = '<a href="' + group.id + '">' + group.name + '</a>';
+            tableRowElement.appendChild(imgContainer);
+            tableRowElement.appendChild(nameContainer);
+            tableElement.appendChild(tableRowElement);
+        });
     };
 }
 
