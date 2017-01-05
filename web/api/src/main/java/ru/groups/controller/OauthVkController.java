251e0ed0411d7fb3e24aad9da53ee9fc51c47bb5
Mello007 <sindikat008@gmail.com>
2017-01-05 14:26:46 +0300

    added http to path

--- web/api/src/main/java/ru/groups/controller/OauthVkController.java
+++ web/api/src/main/java/ru/groups/controller/OauthVkController.java
@@ -22,12 +22,12 @@ public class OauthVkController {
 
     @RequestMapping(value = "vk", method = RequestMethod.GET)
     public void sendLink(HttpServletResponse response) throws IOException{
-        response.sendRedirect("https://oauth.vk.com/authorize?client_id=5499487&display=page&scope=groups,messages,photos,docs&redirect_uri=89.223.28.35:8080/api-1.0-SNAPSHOT/oauth/token&scope=offline&response_type=code&v=5.52");
+        response.sendRedirect("https://oauth.vk.com/authorize?client_id=5499487&display=page&scope=groups,messages,photos,docs&redirect_uri=http://89.223.28.35:8080/api-1.0-SNAPSHOT/oauth/token&scope=offline&response_type=code&v=5.52");
     }
 
     @RequestMapping(value = "token", method = RequestMethod.GET)
     public void getToken(@RequestParam String code, HttpServletResponse response) throws Exception {
         vkInformationService.loadUserByCode(code);
-        response.sendRedirect("89.223.28.35:8080/api-1.0-SNAPSHOT/admin.html");
+        response.sendRedirect("http://89.223.28.35:8080/api-1.0-SNAPSHOT/admin.html");
     }
 }
