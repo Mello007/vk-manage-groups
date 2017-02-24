0825b02f14fd5e4b9f3bc83c4d742c1f1fe64afe
Mello007 <sindikat008@gmail.com>
2017-02-24 18:51:07 +0300

    added simple controller for vk call back

---
+++ web/api/src/main/java/ru/groups/controller/VkCallBackController.java
@@ -0,0 +1,18 @@
+package ru.groups.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ru.groups.entity.DTO.UserDTO;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value = "call")
+public class VkCallBackController {
+
+    @RequestMapping(value = "get", method = RequestMethod.POST)
+    public String get(){
+        return "a975253d";
+    }
+}
